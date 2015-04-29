d1834ea7526c9300a74968a39f3c4e53d872aec8
Alex Mason <Alexander.Mason@nicta.com.au>
2015-04-29 16:14:31 +1000

    Reindent and cleanup

--- src/Util/Charts.hs
+++ src/Util/Charts.hs
@@ -1,18 +1,17 @@
 module Util.Charts where
 
+import           Data.Colour.SRGB              (sRGB24read)
 import           Graphics.Rendering.Chart.Easy
-import           Data.Colour.SRGB                          (sRGB24read)
 
-import           Data.Time.Clock                           (UTCTime)
-import           Data.Time.LocalTime                       (LocalTime (..),
-                                                            TimeZone,
-                                                            timeZoneName,
-                                                            utcToLocalTime)
+import           Data.Time.Clock               (UTCTime)
+import           Data.Time.LocalTime           (LocalTime (..), TimeZone,
+                                                utcToLocalTime)
 
-import           Data.Text (Text)
-import qualified Data.Text as T
 
-import Control.Monad
+import           Data.Text                     (Text)
+import qualified Data.Text                     as T
+
+import           Control.Monad
 
 
 -- From http://www.mulinblog.com/a-color-palette-optimized-for-data-visualization/
@@ -28,35 +27,32 @@ colours = map (opaque . sRGB24read) [
     "#4D4D4D",
     "#F15854"]
 
+
+-- TODO: Remove the time specific stuff
 createContributionChart
-  :: TimeZone
-  -> [(Text,[Maybe (UTCTime,Double)])]
-  -> EC (Layout LocalTime Double) ()
-  -> Renderable ()
-createContributionChart tz vss settings =
- -- toFile def file $
- toRenderable $
-    do
+    :: TimeZone
+    -> [(Text,[Maybe (UTCTime,Double)])]
+    -> EC (Layout LocalTime Double) ()
+    -> Renderable ()
+createContributionChart tz vss settings = toRenderable $ do
+    layout_background                             .= solidFillStyle (opaque white)
+    layout_foreground                             .= (opaque black)
+    -- layout_left_axis_visibility . axis_show_ticks .= False
+    layout_legend . _Just . legend_orientation    .= LOCols 1
+    layout_legend . _Just . legend_margin         .= 10
+    -- layout_x_axis . laxis_style . axis_label_gap .= 10
+    -- layout_margin .= 100
+    setColors colours
 
-      layout_background                             .= solidFillStyle (opaque white)
-      layout_foreground                             .= (opaque black)
-      -- layout_left_axis_visibility . axis_show_ticks .= False
-      layout_legend . _Just . legend_orientation    .= LOCols 1
-      layout_legend . _Just . legend_margin         .= 10
-      layout_x_axis . laxis_title                   .= timeZoneName tz
-      -- layout_x_axis . laxis_style . axis_label_gap .= 10
-      -- layout_margin .= 100
-      setColors colours
+    settings
 
-      settings
-
-      forM_ vss $ \(name,vs) ->
-          plot . liftEC $ do
-                colour <- takeColor
-                plot_lines_title .= (T.unpack name)
-                plot_lines_values .= [ [ (utcToLocal d,v) | Just (d,v) <- vs] ]
-                plot_lines_style . line_color .= colour
-                plot_lines_style . line_width .= 3
+    forM_ vss $ \(name,vs) ->
+        plot . liftEC $ do
+            colour <- takeColor
+            plot_lines_title .= (T.unpack name)
+            plot_lines_values .= [ [ (utcToLocal d,v) | Just (d,v) <- vs] ]
+            plot_lines_style . line_color .= colour
+            plot_lines_style . line_width .= 3
     where
         utcToLocal :: UTCTime -> LocalTime
         utcToLocal lt = utcToLocalTime tz lt
