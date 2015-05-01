1ff9252cf9d3c5c662d367921361d21fc73c136a
Alex Mason <Alexander.Mason@nicta.com.au>
2015-05-01 13:47:38 +1000

    Create AEMO live solar web service

--- src/AEMO/LivePower.hs
+++ src/AEMO/LivePower.hs
@@ -2,32 +2,46 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeOperators     #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 module AEMO.LivePower where
 
-import           Control.Applicative
-import Control.Lens hiding ((.=))
+-- import           Control.Applicative
+import           Control.Lens
 
-import           Data.Text            (Text)
--- import qualified Data.Text as T
+import           Data.Text                                 (Text)
+import qualified Data.Text                                 as T
 --
-import           Data.HashMap.Strict  (HashMap)
-import qualified Data.HashMap.Strict  as H
+import           Data.Hashable
+import           Data.HashMap.Strict                       (HashMap)
+import qualified Data.HashMap.Strict                       as H
 
-import           Data.ByteString      (ByteString)
-import qualified Data.ByteString      as B
-
-
-import           Data.Maybe           (catMaybes)
+-- import           Data.ByteString      (ByteString)
+-- import qualified Data.ByteString      as B
 
 
-import           Data.IORef           (IORef, readIORef, writeIORef, newIORef)
+import           Data.Maybe                                (catMaybes)
 
-import           Data.Csv
+import           Data.Monoid
 
-import           Control.Exception    (throw)
+import           Data.IORef                                (IORef, newIORef,
+                                                            readIORef,
+                                                            writeIORef)
+
+import           Data.Time.Clock                           (UTCTime)
+
+import           Data.Time.LocalTime
+
+import           Control.Monad.IO.Class
+
+import           Data.Csv                                  (NamedRecord,
+                                                            encodeByName,
+                                                            namedRecord,
+                                                            toNamedRecord)
+import qualified Data.Csv                                  as C
+
+import           Control.Exception                         (throw)
 
 import           Database.Persist
 -- import Database.Persist.Class
@@ -37,31 +51,44 @@ import           Servant
 import           AEMO.Database
 import           AEMO.Types
 -- import           System.Log.FastLogger
-import           Control.Monad.Logger (LogLevel (..))
+import           Control.Monad.Logger                      (LogLevel (..))
 
-import Data.Default
+import           Data.Default
 
+import           Util.Periodic
 import           Util.Types
 import           Util.Web
-import           Util.Periodic
+
+import           Graphics.Rendering.Chart.Backend.Diagrams (renderableToSVGString)
+import           Graphics.Rendering.Chart.Easy
+import           Util.Charts
+
+import           Network.HTTP.Types.Status                 (status200)
+import           Network.Wai                               (Application)
+import           Network.Wai.Util                          (bytestring)
+
+
+
 
 type AEMOLivePower =
     "csv" :> Raw
-    -- :<|>
-    -- Capture "DUID" Text :> "svg" :> Raw
+    :<|>
+    Capture "DUID" Text :> "svg" :> Raw
 
 
 
 
-data ALPState = ALPS {
-    _powerStationLocs :: Maybe (Text -> CsvBS)
+data ALPState = ALPS
+    { _powerStationLocs :: Maybe (Text -> CsvBS)
+    -- , _psSvgs           :: HashMap Text CsvBS
 }
 
 $(makeLenses ''ALPState)
 
 instance Default ALPState where
-    def = ALPS {
-        _powerStationLocs = Nothing
+    def = ALPS
+        { _powerStationLocs = Nothing
+        -- , _psSvgs = H.empty
         }
 
 
@@ -73,7 +100,8 @@ makeAEMOLivePowerServer = do
     if success
         then do
             _tid <- updateALPState ref `every` (5 :: Minute)
-            return . Right $ (serveCSV ref powerStationLocs)
+            return . Right $ (serveCSV ref powerStationLocs
+                             :<|> serveSVGLive)
         else return . Left $ "The impossible happened!"
 
 updateALPState :: IORef ALPState -> IO Bool
@@ -84,6 +112,17 @@ updateALPState ref = do
     writeIORef ref $ current
         & powerStationLocs .~ Just csvf
     return True
+
+
+serveSVGLive :: Text -> Application
+serveSVGLive duid _req resp = do
+     evs <- runApp dbConn 1 LevelDebug (getPSDForToday duid)
+     case evs of
+        Left err -> error (show err)
+        Right vs -> do
+             chrt <- makePSDChart duid vs
+             (svg',_) <- liftIO $ renderableToSVGString chrt 500 300
+             resp =<< bytestring status200 [("Content-Type", "image/svg+xml")] svg'
 
 getLocs :: IO ([Entity DuidLocation],[Entity PowerStation],[Entity PowerStationDatum])
 getLocs = do
@@ -109,10 +148,10 @@ makeCsv :: [Entity DuidLocation]
 makeCsv locs pows dats = let
     toLocRec :: DuidLocation -> NamedRecord
     toLocRec dloc = namedRecord
-        [ "DUID"    .= duidLocationDuid dloc
-        , "Lat"     .= duidLocationLat dloc
-        , "Lon"     .= duidLocationLon dloc
-        , "comment" .= duidLocationComment dloc
+        [ "DUID"    C..= duidLocationDuid dloc
+        , "Lat"     C..= duidLocationLat dloc
+        , "Lon"     C..= duidLocationLon dloc
+        , "comment" C..= duidLocationComment dloc
         ]
 
     powsByDuid :: HashMap Text NamedRecord
@@ -155,18 +194,18 @@ makeCsv locs pows dats = let
         , "comment"
         ]
 
-    replaceKey :: ByteString -> ByteString -> HashMap ByteString a -> HashMap ByteString a
-    replaceKey from to mp = case H.lookup from mp of
+    replaceKey :: (Hashable k, Eq k) => k -> k -> HashMap k a -> HashMap k a
+    replaceKey frm to mp = case H.lookup frm mp of
         Nothing -> mp
-        Just v  -> H.insert to v (H.delete from mp)
+        Just v  -> H.insert to v (H.delete frm mp)
 
-    empty :: Text
-    empty = "-"
+    emptyT :: Text
+    emptyT = "-"
 
     emptyDatum :: NamedRecord
     emptyDatum = namedRecord
-        [ "Most Recent Output (MW)" .= empty
-        , "Sample Time UTC" .= empty
+        [ "Most Recent Output (MW)" C..= emptyT
+        , "Sample Time UTC" C..= emptyT
         ]
 
     !csv = unchunkBS $
@@ -185,3 +224,31 @@ makeCsv locs pows dats = let
 
 
     in \_host -> CsvBS $! csv
+
+
+-- getPSDForToday :: Text -> AppM (Maybe SvgBS)
+getPSDForToday :: Text -> AppM [Entity PowerStationDatum]
+getPSDForToday duid = do
+    now <- liftIO getZonedTime
+    let today :: ZonedTime
+        today = now {zonedTimeToLocalTime =
+                     (zonedTimeToLocalTime now) {localTimeOfDay = midnight}
+                    } -- need lenses :(
+        todayUTC :: UTCTime
+        todayUTC = zonedTimeToUTC today
+
+    runDB $ do
+        selectList [PowerStationDatumDuid ==. duid
+                   ,PowerStationDatumSampleTime >=. todayUTC]
+                   []
+
+makePSDChart :: Text -> [Entity PowerStationDatum] -> IO (Renderable ())
+makePSDChart duid es = do
+    tz <- liftIO getCurrentTimeZone
+    let psds = map entityVal es
+        tvs = map (\psd -> (powerStationDatumSampleTime psd, powerStationDatumMegaWatt psd)) psds
+        lvs = map (\(t,v) -> (utcToLocalTime tz t, v)) tvs
+    return $ wsChart [(duid,lvs)] $ do
+                layout_title .= (T.unpack ("Production for " <> duid))
+                layout_y_axis . laxis_title .= "MW"
+                layout_x_axis . laxis_title .= timeZoneName tz
