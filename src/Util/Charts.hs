73cc864026c0b45336c4808874e1949a4ec262b0
Alex Mason <Alex.Mason@nicta.com.au>
2016-03-31 16:29:22 +1100

    Change configuration from configurator to configuration-tools

--- src/Util/Charts.hs
+++ src/Util/Charts.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Util.Charts where
 
@@ -15,17 +17,10 @@ import           Graphics.Rendering.Chart.Backend.Diagrams (DEnv, DFont,
                                                             FontSelector,
                                                             runBackendR)
 
-#if MIN_VERSION_diagrams_lib(1,3,0)
 import           Diagrams.TwoD.Size                        (mkSizeSpec2D)
-#else
-import           Diagrams.TwoD.Size                        (mkSizeSpec)
-#endif
 
 import           Data.Text                                 (Text)
 import qualified Data.Text                                 as T
-
-import qualified Data.Configurator                         as C
-import           Data.Configurator.Types                   (Config)
 
 import qualified Graphics.SVGFonts.ReadFont                as F
 
@@ -35,22 +30,40 @@ import           Control.Monad
 
 import           Util.Types
 
+import           Configuration.Utils hiding ((.=))
+import qualified Configuration.Utils as C
+-- import Control.Lens
+
+newtype FontConfig = FontConfig {_fontPath :: String} deriving (Show, Eq)
+$(makeLenses ''FontConfig)
+
+instance FromJSON (FontConfig -> FontConfig) where
+  parseJSON = withObject "FontConfig" $ \o -> id <$< fontPath ..: "fontPath" % o
+
+instance ToJSON FontConfig where
+  toJSON a = object
+      [ "fontPath" C..= _fontPath a]
+
+pAuth :: MParser FontConfig
+pAuth = id
+    <$< fontPath .:: strOption
+        % long "fontsdir"
+        <> help "Directory containing SVG fonts for"
+
+defaultFontConfig :: FontConfig
+defaultFontConfig = FontConfig "./fonts"
+
+
+
 -- From http://www.mulinblog.com/a-color-palette-optimized-for-data-visualization/
 colours :: [AlphaColour Double]
 colours = map (opaque . sRGB24read) [
-    "#5DA5DA",
-    "#FAA43A",
-    "#60BD68",
-    "#F17CB0",
-    "#B2912F",
-    "#B276B2",
-    "#DECF3F",
-    "#4D4D4D",
+    "#5DA5DA", "#FAA43A", "#60BD68", "#F17CB0",
+    "#B2912F", "#B276B2", "#DECF3F", "#4D4D4D",
     "#F15854"]
 
 
 
-#if MIN_VERSION_diagrams_lib(1,3,0)
 renderImage :: DEnv Double -> Double -> Double -> Renderable () -> IO (Image PixelRGBA8)
 renderImage env w h r = do
     -- env <- # SCC "renderImage.defaultEnv" # defaultEnv bitmapAlignmentFns h w
@@ -58,23 +71,7 @@ renderImage env w h r = do
         !img = renderDia Rasterific (RasterificOptions (mkSizeSpec2D (Just w) (Just h))) dia
     return img
 
-#else
-
-renderImage :: DEnv -> Double -> Double -> Renderable () -> IO (Image PixelRGBA8)
-renderImage env w h r = do
-    -- env <- # SCC "renderImage.defaultEnv" # defaultEnv bitmapAlignmentFns h w
-    let (!dia,_) = runBackendR env r
-        !img = renderDia Rasterific (RasterificOptions (mkSizeSpec (Just w) (Just h))) dia
-    return img
-#endif
-
-
-
-#if MIN_VERSION_diagrams_lib(1,3,0)
 type ChartEnv = DEnv Double
-#else
-type ChartEnv = DEnv
-#endif
 
 isFontFamily :: String -> DFont n -> Bool
 isFontFamily n (fd, _) = n == F.fontDataFamily fd
@@ -82,16 +79,14 @@ isFontFamily n (fd, _) = n == F.fontDataFamily fd
 alterFontFamily :: String -> DFont n -> DFont n
 alterFontFamily n (fd, om) = (fd { F.fontDataFamily = n }, om)
 
-loadFonts :: Config -> IO (FontSelector Double)
-loadFonts conf = do
-    fontPath <- C.lookupDefault "fonts" conf "font-path"
-
+loadFonts :: FontConfig -> IO (FontSelector Double)
+loadFonts fconf = do
     [
        sansR
      , sansRB
      , sansRBI
      , sansRI
-     ] <- mapConcurrently (F.loadFont . (fontPath ++))
+     ] <- mapConcurrently (F.loadFont . (_fontPath fconf ++))
                     [ "/SourceSansPro_R.svg"
                     , "/SourceSansPro_RB.svg"
                     , "/SourceSansPro_RBI.svg"
@@ -127,11 +122,11 @@ wsChart vss settings =
 
         _ | any (null . snd) vss
                 -> error $ "empty chart series for: " ++ show (map fst . filter (null . snd) $ vss)
-          | any (\(_,(_:xs)) -> null xs) vss
-                -> error $ "cannot make chart of a single data point for: " ++ show (map fst . filter (\(_,(_:xs)) -> null xs) $ vss)
+          | any (\(_,_:xs) -> null xs) vss
+                -> error $ "cannot make chart of a single data point for: " ++ show (map fst . filter (\(_,_:xs) -> null xs) $ vss)
           | otherwise -> toRenderable $ do
             layout_background                             .= solidFillStyle (opaque white)
-            layout_foreground                             .= (opaque black)
+            layout_foreground                             .= opaque black
             -- layout_left_axis_visibility . axis_show_ticks .= False
             layout_legend . _Just . legend_orientation    .= LOCols 1
             layout_legend . _Just . legend_margin         .= 10
@@ -144,7 +139,7 @@ wsChart vss settings =
             forM_ vss $ \(name,vs) ->
                 plot . liftEC $ do
                     colour <- takeColor
-                    plot_lines_title .= (T.unpack name)
+                    plot_lines_title .= T.unpack name
                     plot_lines_values .= [ vs ]
                     plot_lines_style . line_color .= colour
                     plot_lines_style . line_width .= 2
